e34159b323c14459413c28e50a79f3ca97faba4e
Chongliang-Utopia <50967591+Chongliang-Utopia@users.noreply.github.com>
2020-08-02 12:15:30 -0400

    add controller

---
+++ src/cs5004/animator/controller/IController.java
@@ -0,0 +1,15 @@
+package cs5004.animator.controller;
+
+
+import java.io.IOException;
+
+/**
+ * A controller interface that enables the program to output the description of the shapes, or draws
+ * the animation on a canvas.
+ */
+public interface IController {
+  /**
+   * The method that will render the text, or draw the shapes.
+   */
+  void run(Appendable ap) throws  IOException;
+}
