a382b926ca68eb20f92acf98f1b0f30718aa3d44
liulu <liu.lu3@northeastern.edu>
2020-08-03 20:30:12 -0400

    update comments

--- src/cs5004/animator/view/FactoryView.java
+++ src/cs5004/animator/view/FactoryView.java
@@ -5,9 +5,16 @@ package cs5004.animator.view;
  */
 public class FactoryView {
   /**
-   * a factory method that renders the corresponding view according to the view type and the tempo.
+   * A factory method that renders the corresponding view according to the view type and the tempo.
+   *
+   * @param type the type of the view want to generate
+   * @return the IView object
+   * @throws IllegalArgumentException if the input is invalid view
    */
   public static IView makeView(String type) throws IllegalArgumentException {
+    if (type == null) {
+      throw new IllegalArgumentException("Not a valid view");
+    }
     switch (type) {
       case "text":
         return new TextView();
